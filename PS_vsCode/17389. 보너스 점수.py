5afa42962921a5242b3fb14a0782881ed70b6ac5
TSCKANU <sunghyun7949@naver.com>
2020-09-17 02:23:48 +0900

    튜플

---
+++ PS_vsCode/17389. 보너스 점수.py
@@ -0,0 +1,10 @@
+N, S = int(input()), input()
+
+score, bonus = 0, 0
+
+for idx, OX in enumerate(S):
+    if OX == 'O':
+        score, bonus = score + (idx + 1) + bonus, bonus + 1
+    else:
+        bonus = 0
+print(score)
