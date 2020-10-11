fad3e642115ad420e6a8a86ec87a58a5bf6a5396
TSCKANU <sunghyun7949@naver.com>
2020-10-11 23:51:02 +0900

    DFS, BFS, 구현, 시뮬레이션

--- PS_vsCode/17142. 연구소 3.py
+++ PS_vsCode/17142. 연구소 3.py
@@ -1,7 +1,67 @@
+from collections import deque
+
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+select = [False] * 10
+q, v, k = deque(), [], 0
+
+def bfs(dist):
+    global ans
+    infect, times = 0, 0
+    while q:
+        y, x = q.popleft()
+        for dy, dx in (-1, 0), (1, 0), (0, 1), (0, -1):
+            ny, nx = y + dy, x + dx
+            if ny < 0 or ny >= n or nx < 0 or nx >= n:
+                continue
+
+            if a[ny][nx] != 1 and dist[ny][nx] == -1:
+                dist[ny][nx] = dist[y][x] + 1
+                q.append((ny, nx))
+                if a[ny][nx] == 0:
+                    infect += 1
+                    times = dist[ny][nx]
+
+    for i in dist:
+        print(i)
+    print()
+    if infect == k:
+        ans = min(ans, times)
+
+
+def solve(idx, cnt, d):
+    if cnt == m:
+        dist = [[-1] * n for _ in range(n)]
+        for i in range(d):
+            if select[i]:
+                y, x = v[i]
+                q.append((y, x))
+                dist[y][x] = 0
+        bfs(dist)
+        return
+    
+    for i in range(idx, d):
+        if not select[i]:
+            select[i] = True
+            solve(i+1, cnt+1, d)
+            select[i] = False
+
+
+for i in range(n):
+    for j in range(n):
+        if a[i][j] == 2:
+            v.append((i, j))
+        if a[i][j] == 0:
+            k += 1
+
+ans = 10**9
+solve(0, 0, len(v))
+print(ans if ans != 10**9 else -1)
+
+'''
 import copy
 from itertools import combinations
 from collections import deque
-
 
 def bfs(lst, virus):
     lst = copy.deepcopy(lst)
@@ -79,3 +139,4 @@ if none_count == len(viruses):
     result = -1
 
 print(result)
+'''
