122065048a749a9ccf897ee559cbe7ad3e3a31cb
TSCKANU <sunghyun7949@naver.com>
2020-10-16 14:16:08 +0900

    구현, 시뮬레이션

--- PS_vsCode/17143. 낚시왕.py
+++ PS_vsCode/17143. 낚시왕.py
@@ -3,8 +3,9 @@
 방향, 속도
 이동 마친 후에는 상어가 두마리 이상 있을 수 있음 --> 크기가 가장 큰 상어만 남는다.
 '''
+## 성공이지만 각 상어의 속력만큼 반복문을 돌려서 상어를 이동시키므로 시간이 많이 소요.
+# 반복문을 안쓰는 방법은 밑의 주석 코드에 있음.(너무 복잡함,,,)
 from collections import deque
-import heapq
 
 R, C, M = map(int, input().split())
 board = [[0 for _ in range(C+2)] for _ in range(R+1)]
@@ -77,3 +78,106 @@ while fishing_x != C+1:
     # break
 
 print(result)
+
+'''
+# 각 상어의 속력과 방향을 알기 때문에 반복문을 사용하지 않고 조건문으로 한 번에 이동하도록 구현
+import sys
+input = sys.stdin.readline
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def dir(d):
+    if d == 1: return 2
+    elif d == 2: return 1
+    elif d == 3: return 4
+    else: return 3
+
+r, c, m = map(int, input().split())
+a = [[0] * c for _ in range(r)]
+q = []
+
+for _ in range(m):
+    x, y, s, d, z = map(int, input().split())
+    a[x-1][y-1] = [s, d, z]
+    q.append([x-1, y-1])
+
+ans, rx, ry = 0, -1, -1
+
+for col in range(c):
+    for i in range(r):
+        if a[i][col]:
+            ans += a[i][col][2]
+            a[i][col] = 0
+            rx, ry = i, col
+            break
+
+    qlen = len(q)
+    temp = [[0] * c for _ in range(r)]
+    q2 = []
+
+    for i in range(qlen):
+        x, y = q[i]
+        if x == rx and y == ry:
+            continue
+        s, d, z = a[x][y][0], a[x][y][1], a[x][y][2]
+
+        if d == 1 or d == 2:
+            nx, ny = x + s * dx[d-1], y
+            if not 0 <= nx < r:
+                temp_s = s
+                if d == 1:
+                    s -= x
+                    x = 0
+                else:
+                    s -= r-1-x
+                    x = r-1
+                d = dir(d)
+                f, g = s // (r-1), s % (r-1)
+                if f % 2 == 0:
+                    if x == 0:
+                        nx = g
+                    else:
+                        nx = r-1-g
+                else:
+                    if x == 0:
+                        nx = r-1-g
+                    else:
+                        nx = g
+                    d = dir(d)
+                s = temp_s
+        else:
+            nx, ny = x, y + s * dy[d-1]
+            if not 0 <= ny < c:
+                temp_s = s
+                if d == 3:
+                    s -= c-1-y
+                    y = c-1
+                else:
+                    s -= y
+                    y = 0
+                d = dir(d)
+                f, g = s // (c-1), s % (c-1)
+                if f % 2 == 0:
+                    if y == 0:
+                        ny = g
+                    else:
+                        ny = c-1-g
+                else:
+                    if y == 0:
+                        ny = c-1-g
+                    else:
+                        ny = g
+                    d = dir(d)
+                s = temp_s
+
+        if temp[nx][ny]:
+            if z > temp[nx][ny][2]:
+                temp[nx][ny] = [s, d, z]
+        else:
+            temp[nx][ny] = [s, d, z]
+            q2.append([nx, ny])
+    a = temp
+    q = q2
+
+print(ans)
+'''
