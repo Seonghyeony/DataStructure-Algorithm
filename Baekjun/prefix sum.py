24c1bf91ba8eeff5acc1821b15309c5b8f2e3e92
Seonghyeony <sunghyun7949@naver.com>
2021-04-23 20:57:33 +0900

    구간 합

---
+++ Baekjun/prefix sum.py
@@ -0,0 +1,14 @@
+n = 5
+data = [10, 20, 30, 40, 50]
+
+# Prefix sum 계산
+summary = 0
+prefix_sum = [0]
+for i in data:
+    summary += i
+    prefix_sum.append(summary)
+
+# 구간 합 계산
+left = 3
+right = 4
+print(prefix_sum[right] - prefix_sum[left - 1])
