4121289cafd0050bda408934fcb14d88052c956f
Seonghyeony <sunghyun7949@naver.com>
2021-05-08 19:31:05 +0900

    intern

--- Baekjun/21609. 상어 중학교.py
+++ Baekjun/21609. 상어 중학교.py
@@ -1,2 +1,48 @@
-# 검은색 블록: -1, 무지개 블록: 0, 일반 블록(M가지 색상).
-# 그룹에는 일반 블록이 적어도 하나 있어야 한다.
+def solution(n, k, cmd):
+    lst = [1 for _ in range(n)]
+    answer = ''
+    st = []
+    
+    for c in cmd:
+        command = c.split(" ")
+        if command[0] == 'D':
+            count = int(command[1])
+            for _ in range(count):
+                temp = lst[k+1:]
+                idx = temp.index(1)
+                k = k + idx + 1
+        elif command[0] == 'U':
+            count = int(command[1])
+            for _ in range(count):
+                temp = lst[:k]
+                temp.reverse()
+                idx = temp.index(1)
+                index = len(temp) - 1 - idx
+                k = index
+        elif command[0] == 'C':
+            lst[k] = 0
+            st.append(k)
+            isEnd = sum(lst[k+1:])
+            # 선택된 행이 마직막 행일 경우
+            if isEnd == 0:
+                temp = lst[:k]
+                temp.reverse()
+                idx = temp.index(1)
+                index = len(temp) - 1 - idx
+                k = index
+            else:
+                temp = lst[k+1:]
+                idx = temp.index(1)
+                k = k + idx + 1
+        elif command[0] == 'Z':
+            row = st.pop()
+            lst[row] = 1
+    
+    for i in range(n):
+        if lst[i]:
+            answer += 'O'
+        else:
+            answer += 'X'
+    return answer
+
+print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]))
